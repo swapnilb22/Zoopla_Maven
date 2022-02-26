89c1d0b050ad9f5497e19043e52b6a8fdd0607ae
Swapnil Bhaskar <41773730+swapnilb22@users.noreply.github.com>
2022-02-26 19:03:54 +0530

    Update TestAgentDetailsofProperty.java
    dev branch first commit

--- src/test/java/com/zoopla/qa/testCases/TestAgentDetailsofProperty.java
+++ src/test/java/com/zoopla/qa/testCases/TestAgentDetailsofProperty.java
@@ -5,7 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
+// this is dev branch code
 import com.zoopla.qa.base.TestBase;
 import com.zoopla.qa.pages.AgentDetailsPage;
 import com.zoopla.qa.pages.PropertyDetailsPage;
